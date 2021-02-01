68fc22becfc97a37185c55d4d0957c4a38a436ac
xperonnaud <x.peronnaud@gmail.com>
2021-02-01 14:17:21 -0500

    Refactored Item DeleteDialog

--- src/components/elements/FormDialog/DialogAppBar.vue
+++ src/components/elements/FormDialog/DialogAppBar.vue
@@ -35,99 +35,16 @@
 
       <v-spacer />
 
-      <v-dialog
+      <x-delete-dialog
         v-if="formDialogType === 'update'"
-        v-model="deleteDialog"
-        :max-width="isMobile ? '300' : '450'"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            v-bind:class="[{'mr-1':!isMobile}]"
-            v-bind="attrs"
-            v-on="on"
-            :disabled="editMode || !isFormMounted"
-            icon
-          >
-            <v-icon :small="editMode" v-text="'mdi-trash-can-outline'" />
-          </v-btn>
-        </template>
-
-        <v-card :style="'border-top: 2px solid '+errorColor+' !important;'">
-          <div class="max-width d-flex align-center justify-center">
-            <v-avatar :size="48" :color="darkColor('red')" style="position: absolute; top: -26px;">
-              <v-icon :size="20" :color="reversedShadeColor" v-text="'mdi-alert'" style="margin-top: 20px;" />
-            </v-avatar>
-          </div>
-
-          <v-card-title class="headline mt-3">
-            <span v-text="`Deleting ${currentRouteTitle}:`" />
-            <span v-html="'&nbsp;'" />
-            <span v-if="hasItemRelations" v-text="' Not Allowed.'" />
-            <span v-else v-text="item.title" />
-          </v-card-title>
-
-          <v-card-subtitle v-if="hasItemRelations">
-            <span v-text="'Remove relations first:'" />
-          </v-card-subtitle>
-
-          <v-card-text>
-            <v-responsive
-                v-if="hasItemRelations"
-                class="overflow-y-auto"
-                :max-height="300"
-            >
-              <v-sheet :color="'grey '+(isDark ? 'darken-4' : 'lighten-4')" class="pa-2">
-                <relation-chip
-                  :relationType="'inventories'"
-                  :list="inventoriesList"
-                ></relation-chip>
-
-                <relation-chip
-                  :relationType="'gear'"
-                  :list="gearList"
-                ></relation-chip>
-
-                <v-divider
-                    v-if="selectedItemRelations.gear && selectedItemRelations.gear.length > 0
-                        && selectedItemRelations.adventures && selectedItemRelations.adventures.length > 0"
-                ></v-divider>
-
-                <relation-chip
-                  :relationType="'adventures'"
-                  :list="adventuresList"
-                ></relation-chip>
-              </v-sheet>
-            </v-responsive>
-
-            <div v-else>
-              <div v-text="'Are you sure your wish to go forward with this?'" />
-              <div v-bind:class="[fontShadeColor]"  v-text="'This action is irreversible.'" />
-            </div>
-          </v-card-text>
-
-          <v-divider />
-
-          <v-card-actions>
-            <v-btn
-              @click="deleteDialog = false"
-              depressed
-              text
-              v-text="'Cancel'"
-            ></v-btn>
-
-            <v-spacer />
-
-            <v-btn
-              :color="darkColor('error')"
-              @click="deleteAction()"
-              :disabled="hasItemRelations"
-              depressed
-            >
-              <span v-bind:class="[reversedFontShadeColor]" v-text="'Delete'" />
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+        v-bind:item.sync="item"
+        v-bind:value.sync="deleteDialog"
+        v-bind:editMode.sync="editMode"
+        v-bind:isFormMounted.sync="isFormMounted"
+        v-bind:isFormValid.sync="isFormValid"
+        v-bind:hasItemRelations.sync="hasItemRelations"
+        v-bind:deleteItem.sync="dialogDeleteItem"
+      ></x-delete-dialog>
 
       <v-btn
         @click="formDialogAction()"
@@ -146,12 +63,12 @@
 
 <script>
 
-  import RelationChip from "@/components/elements/FormDialog/RelationChip";
+  import XDeleteDialog from "@/components/elements/DeleteDialog/XDeleteDialog";
 
   export default {
     name: "dialog-app-bar",
     components: {
-      RelationChip
+      XDeleteDialog
     },
     props: {
       item: {
@@ -195,7 +112,7 @@
         dialogEditMode: this.editMode,
         dialogPostItem: this.postItem,
         dialogPatchItem: this.patchItem,
-        dialogDeleteItem: this.deleteItem,
+        dialogDeleteItem: false,
       }
     },
     computed: {
@@ -208,20 +125,12 @@
       apiPatchItem() {
         this.dialogPatchItem = true;
       },
-      apiDeleteItem() {
-        this.dialogDeleteItem = true;
-      },
       leftArrowAction() {
         if(this.dialogEditMode === true) {
           this.dialogEditMode = false;
         } else {
           this.formDialog = false;
         }
-      },
-      deleteAction() {
-        this.deleteDialog = false;
-        if(this.item)
-          this.apiDeleteItem();
       },
       formDialogAction() {
         if(this.formDialogType === 'post') {
