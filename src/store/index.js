72595a3b44ac422fc4a809b6baf70b13162d9558
xperonnaud <x.peronnaud@gmail.com>
2021-02-01 00:50:47 -0500

    baseUrl => prod

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
@@ -11,7 +11,7 @@ export default new Vuex.Store({
             password: '',
             accessToken: null,
             refreshToken: null,
-            baseUrl: 'http://localhost:8055/',
+            baseUrl: 'https://servo-13475.nodechef.com/',
             isTokenRefreshed: false
         },
         selfio: {
