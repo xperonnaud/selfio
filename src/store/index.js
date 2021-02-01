89c5ac1d89b2336f92c2a7559ce0eed93afdc7ad
xperonnaud <x.peronnaud@gmail.com>
2021-02-01 00:51:40 -0500

    config => local

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
         api: {
             authTimer: null,
-            login: '',
-            password: '',
+            login: 'reivax@gmail.com',
+            password: 'poppers78',
             accessToken: null,
             refreshToken: null,
             baseUrl: 'http://localhost:8055/',
