17603d436f6c4e672554bb467c24466575ae8816
reivax <x.peronnaud@gmail.com>
2021-02-20 20:38:59 -0500

    FIXES :: URLs

--- src/mixins/api.js
+++ src/mixins/api.js
@@ -15,7 +15,7 @@ class MemoryStore {
     }
 }
 
-const directus = new DirectusSDK('http://localhost:8055/', {
+const directus = new DirectusSDK('https://servo-13475.nodechef.com/', {
     auth: {
         storage: new MemoryStore(), // Storage adapter where refresh tokens are stored in JSON mode
         mode: 'json', // What login mode to use. One of `json`, `cookie`
