8e3d76dce22cb90236853b5f982171a8f1b854dd
reivax <x.peronnaud@gmail.com>
2021-03-02 18:44:04 -0500

    TESTS :: text gradient color glitch Safari/Edge/chrome/APPLE!!!

--- src/mixins/screen.js
+++ src/mixins/screen.js
@@ -135,6 +135,18 @@ export default {
         },
     },
     methods: {
+        isIOS() {
+            return [
+                'iPad Simulator',
+                'iPhone Simulator',
+                'iPod Simulator',
+                'iPad',
+                'iPhone',
+                'iPod'
+            ].includes(navigator.platform)
+            // iPad on iOS 13 detection
+            || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+        },
         currentWindowHeightOrValue(value) {
             return (this.currentWindowHeight < value ? this.currentWindowHeight : value);
         },
