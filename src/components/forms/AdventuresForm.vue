4ba706f0752719ec63f2276468d55274dc7d0fed
reivax <x.peronnaud@gmail.com>
2021-03-07 09:32:20 -0500

    REFACTORING :: Inv/Adv GearFilterMenus

--- src/components/forms/AdventuresForm.vue
+++ src/components/forms/AdventuresForm.vue
@@ -278,126 +278,10 @@
 
               <v-spacer />
 
-              <div class="mr-1 text-center filter-menu">
-                <v-menu
-                  v-model="gearFilterModeOn"
-                  :close-on-content-click="false"
-                  min-width="333"
-                  max-width="333"
-                  :nudge-width="200"
-                  left
-                >
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-badge
-                      :color="nbActiveFilters ? shadeColor : 'transparent'"
-                      overlap
-                    >
-                      <template v-if="nbActiveFilters" v-slot:badge>
-                      <span
-                        v-bind:class="reversedFontShadeColor"
-                        v-text="nbActiveFilters"
-                      ></span>
-                      </template>
-
-                      <v-btn
-                        @click="gearFilterModeOn = !gearFilterModeOn"
-                        v-bind="attrs"
-                        v-on="on"
-                        icon
-                      >
-                        <v-icon v-text="gearFilterModeOn?'mdi-filter-variant-minus':'mdi-filter-variant'" />
-                      </v-btn>
-                    </v-badge>
-                  </template>
-
-                  <v-card>
-                    <v-list class="py-1">
-                      <v-list-item>
-                        <v-list-item-title>{{$t('global.filters') | capitalizeFirstFilter}}</v-list-item-title>
-
-                        <v-spacer />
-
-                        <v-btn
-                          @click="closeGearFilterMenu()"
-                          icon
-                        >
-                          <v-icon v-text="'mdi-close'" />
-                        </v-btn>
-
-                      </v-list-item>
-                    </v-list>
-
-                    <v-list>
-                      <v-list-item class="mb-3">
-                        <x-category-selector v-bind:value.sync="filters.gearCategoryFilter" isInFilter />
-                      </v-list-item>
-
-                      <v-list-item class="mb-3">
-                        <v-autocomplete
-                          v-if="gearFilterModeOn"
-                          :label="xCapFirst($t('global.tags'))"
-                          v-model="filters.gearTagsFilter"
-                          :items="preferences.gear_tags"
-                          :color="currentColor"
-                          filled
-                          dense
-                          clearable
-                          hide-details="auto"
-                        ></v-autocomplete>
-                      </v-list-item>
-
-                      <v-list-item class="mb-3">
-                        <x-brand-selector v-bind:value.sync="filters.gearBrandFilter" isInFilter />
-                      </v-list-item>
-
-                      <v-list-item class="mb-3">
-                        <x-state-selector v-bind:value.sync="filters.gearStateFilter" isInFilter />
-                      </v-list-item>
-
-                      <v-list-item class="mb-3">
-                        <x-checkbox
-                          label="packed"
-                          v-bind:value.sync="filters.gearIsPackedFilter"
-                        ></x-checkbox>
-                      </v-list-item>
-
-                      <v-list-item class="mb-3">
-                        <x-checkbox
-                          label="worn"
-                          v-bind:value.sync="filters.gearIsWornFilter"
-                        ></x-checkbox>
-                      </v-list-item>
-
-                      <v-list-item class="mb-3">
-                        <x-checkbox
-                          label="consumable"
-                          v-bind:value.sync="filters.gearConsumableFilter"
-                        ></x-checkbox>
-                      </v-list-item>
-                    </v-list>
-
-                    <v-card-actions>
-                      <v-btn
-                        @click.stop="clearAdventureMenuFilters()"
-                        :color="errorColor"
-                        text
-                      >
-                        <span v-text="$t('global.reset')" />
-                      </v-btn>
-
-                      <v-spacer />
-
-                      <v-btn
-                        @click.stop="closeGearFilterMenu()"
-                        icon
-                        class="mr-2"
-                      >
-                        <poly-icon icon="mdi-check" :size="XLI" />
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-menu>
-              </div>
+              <adventure-gear-filter-menu
+                v-bind:filters.sync="filters"
+                v-bind:gearFilterMode.sync="gearFilterModeOn"
+              ></adventure-gear-filter-menu>
 
               <v-btn
                 @click.stop="closeGearList()"
@@ -488,6 +372,7 @@
   const _ = require('lodash');
 
   import Vue from 'vue'
+  import AdventureGearFilterMenu from "@/components/elements/FilterMenu/AdventureGearFilterMenu";
   import AdventureGearListHeader from "@/components/lists/headers/AdventureGearListHeader";
   import PolyIcon from "@/components/elements/Icons/PolyIcon";
   import AdventureGearListItem from "@/components/lists/items/AdventureGearListItem";
@@ -495,37 +380,30 @@
   import XText from "@/components/inputs/XText";
   import XTitleField from "@/components/inputs/XTitleField";
   import XWeatherSelector from "@/components/inputs/XWeatherSelector";
-  import XBrandSelector from "@/components/inputs/XBrandSelector";
-  import XStateSelector from "@/components/inputs/XStateSelector";
   import XIncrement from "@/components/inputs/XIncrement";
   import XDatePicker from "@/components/inputs/XDatePicker";
   import XTimePicker from "@/components/inputs/XTimePicker";
   import XPicker from "@/components/inputs/XPicker";
-  import XCategorySelector from "@/components/inputs/XCategorySelector";
   import XActivitySelector from "@/components/inputs/XActivitySelector";
   import XLandscapeSelector from "@/components/inputs/XLandscapeSelector";
   import XCombobox from "@/components/inputs/XCombobox";
-  import XCheckbox from "@/components/inputs/XCheckbox";
 
   export default {
     name: 'adventures-form',
     components: {
       PolyIcon,
+      AdventureGearFilterMenu,
       AdventureGearListHeader,
       AdventureGearListItem,
       AdventureGearCard,
       XText,
       XTitleField,
-      XCheckbox,
       XWeatherSelector,
-      XBrandSelector,
-      XStateSelector,
       XCombobox,
       XIncrement,
       XPicker,
       XTimePicker,
       XDatePicker,
-      XCategorySelector,
       XActivitySelector,
       XLandscapeSelector,
     },
@@ -591,17 +469,6 @@
       },
     }),
     computed: {
-      nbActiveFilters() {
-        let self = this;
-        let nbActiveFilters = 0;
-
-        Object.keys(this.filters).forEach(function(item) {
-          if(self.filters[item]!==null && self.filters[item]!==undefined && self.filters[item]!==false)
-            nbActiveFilters++;
-        });
-
-        return nbActiveFilters;
-      },
       nbUnpackedItems() {
         if(this.isMounted && this.updatedItem.packed_gear && this.updatedItem.packed_gear.length)
           return (this.originalInventoryGear.length - this.updatedItem.packed_gear.length);
@@ -666,15 +533,6 @@
       },
     },
     methods: {
-      clearAdventureMenuFilters() {
-        this.filters.gearCategoryFilter = null;
-        this.filters.gearTagsFilter = null;
-        this.filters.gearStateFilter = null;
-        this.filters.gearBrandFilter = null;
-        this.filters.gearIsPackedFilter = null;
-        this.filters.gearIsWornFilter = null;
-        this.filters.gearConsumableFilter = null;
-      },
       closeGearFilterMenu() {
         this.gearFilterModeOn = false;
       },
--- src/components/forms/InventoriesForm.vue
+++ src/components/forms/InventoriesForm.vue
@@ -417,126 +417,10 @@
 
             <v-spacer />
 
-            <div class="mr-1 text-center filter-menu">
-              <v-menu
-                v-model="gearFilterModeOn"
-                :close-on-content-click="false"
-                min-width="333"
-                max-width="333"
-                :nudge-width="200"
-                left
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-badge
-                    :color="nbActiveFilters ? shadeColor : 'transparent'"
-                    overlap
-                  >
-                    <template v-if="nbActiveFilters" v-slot:badge>
-                      <span
-                        v-bind:class="reversedFontShadeColor"
-                        v-text="nbActiveFilters"
-                      ></span>
-                    </template>
-
-                    <v-btn
-                      @click="gearFilterModeOn = !gearFilterModeOn"
-                      v-bind="attrs"
-                      v-on="on"
-                      icon
-                    >
-                      <v-icon v-text="gearFilterModeOn ? 'mdi-filter-variant-minus' : 'mdi-filter-variant'" />
-                    </v-btn>
-                  </v-badge>
-                </template>
-
-                <v-card>
-                  <v-list class="py-1">
-                    <v-list-item>
-                      <v-list-item-title>{{$t('global.filters') | capitalizeFirstFilter}}</v-list-item-title>
-
-                      <v-spacer />
-
-                      <v-btn
-                        @click="closeGearFilterMenu()"
-                        icon
-                      >
-                        <v-icon v-text="'mdi-close'" />
-                      </v-btn>
-
-                    </v-list-item>
-                  </v-list>
-
-                  <v-list>
-                    <v-list-item class="mb-3">
-                      <x-category-selector v-bind:value.sync="filters.gearCategoryFilter" isInFilter />
-                    </v-list-item>
-
-                    <v-list-item class="mb-3">
-                      <v-autocomplete
-                        v-if="gearFilterModeOn"
-                        :label="xCapFirst($t('global.tags'))"
-                        v-model="filters.gearTagsFilter"
-                        :items="preferences.gear_tags"
-                        :color="currentColor"
-                        filled
-                        dense
-                        clearable
-                        hide-details="auto"
-                      ></v-autocomplete>
-                    </v-list-item>
-
-                    <v-list-item class="mb-3">
-                      <x-brand-selector v-bind:value.sync="filters.gearBrandFilter" isInFilter />
-                    </v-list-item>
-
-                    <v-list-item class="mb-3">
-                      <x-state-selector v-bind:value.sync="filters.gearStateFilter" isInFilter />
-                    </v-list-item>
-
-                    <v-list-item class="mb-3">
-                      <x-checkbox
-                        label="consumable"
-                        v-bind:value.sync="filters.gearConsumableFilter"
-                      ></x-checkbox>
-                    </v-list-item>
-
-                    <v-list-item class="mb-3">
-                      <x-checkbox
-                        label="packed"
-                        v-bind:value.sync="filters.gearIsPackedFilter"
-                      ></x-checkbox>
-                    </v-list-item>
-
-                    <v-list-item class="mb-3">
-                      <x-checkbox
-                        label="quantity-positive"
-                        v-bind:value.sync="filters.gearQuantityOwnedFilter"
-                      ></x-checkbox>
-                    </v-list-item>
-                  </v-list>
-
-                  <v-card-actions>
-                    <v-btn
-                      @click="clearMenuFilters()"
-                      :color="errorColor"
-                      text
-                    >
-                      <span v-text="$t('global.reset')" />
-                    </v-btn>
-
-                    <v-spacer />
-
-                    <v-btn
-                      @click="closeGearFilterMenu()"
-                      icon
-                      class="mr-2"
-                    >
-                      <poly-icon icon="mdi-check" :size="XLI" />
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-menu>
-            </div>
+            <inventory-gear-filter-menu
+              v-bind:filters.sync="filters"
+              v-bind:gearFilterMode.sync="gearFilterModeOn"
+            ></inventory-gear-filter-menu>
 
             <v-btn
               @click="closeGearList()"
@@ -707,13 +591,11 @@
 
   const _ = require('lodash');
 
+  import InventoryGearFilterMenu from "@/components/elements/FilterMenu/InventoryGearFilterMenu";
   import InventoryGearListHeader from "@/components/lists/headers/InventoryGearListHeader";
   import PolyIcon from "@/components/elements/Icons/PolyIcon";
   import InventoryGearListItem from "@/components/lists/items/InventoryGearListItem";
   import InventoryGearCard from "@/components/elements/Cards/InventoryGearCard";
-  import XCategorySelector from "@/components/inputs/XCategorySelector";
-  import XBrandSelector from "@/components/inputs/XBrandSelector";
-  import XStateSelector from "@/components/inputs/XStateSelector";
   import XTitleField from "@/components/inputs/XTitleField";
   import XCombobox from "@/components/inputs/XCombobox";
   import XSortIcon from "@/components/elements/Icons/XSortIcon";
@@ -727,6 +609,7 @@
   export default {
     name: 'inventories-form',
     components: {
+      InventoryGearFilterMenu,
       InventoryGearListHeader,
       PolyIcon,
       InventoryGearListItem,
@@ -734,9 +617,6 @@
       InventoryGearCard,
       XTitleField,
       XUnknownCategoryIcon,
-      XCategorySelector,
-      XBrandSelector,
-      XStateSelector,
       XIncrement,
       XCheckbox,
       XSortIcon,
@@ -874,17 +754,6 @@
       }
     }),
     computed: {
-      nbActiveFilters() {
-        let self = this;
-        let nbActiveFilters = 0;
-
-          Object.keys(this.filters).forEach(function(item) {
-            if(self.filters[item]!==null && self.filters[item]!==undefined && self.filters[item]!==false)
-              nbActiveFilters++;
-          });
-
-        return nbActiveFilters;
-      },
       mobileResponsiveHeight() {
         return (this.currentWindowHeight - 240);
       },
@@ -964,18 +833,6 @@
         this.temporaryInventoryGear.gear_id = null;
         this.temporaryInventoryGear.gear_quantity_packed = null;
         this.temporaryInventoryGear.gear_worn = null;
-      },
-      clearMenuFilters() {
-        this.filters.gearCategoryFilter = null;
-        this.filters.gearTagsFilter = null;
-        this.filters.gearStateFilter = null;
-        this.filters.gearBrandFilter = null;
-        this.filters.gearConsumableFilter = null;
-        this.filters.gearIsPackedFilter = null;
-        this.filters.gearQuantityOwnedFilter = null;
-      },
-      closeGearFilterMenu() {
-        this.gearFilterModeOn = false;
       },
       inventoryGearItem(gearId) {
         let index = this.inventoryGearList.indexOf(gearId);
